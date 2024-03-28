50c185500a96708bcbd13da094cffe24a3d57cff
Qingyao Sun <qs234@cornell.edu>
2024-03-27 22:51:27 -0400

    introduce scales

--- rp.py
+++ rp.py
@@ -7,19 +7,18 @@ import optax
 from tqdm import trange
 
 
-def encode(samples, alphabet):
+def quantize(permutation, alphabet, scales, samples):
     M, = alphabet.shape
     L = M.bit_length() - 1
-    assert len(samples.shape) == 1
+    assert len(scales.shape) == len(samples.shape) == 1
 
-    permutation = jax.random.permutation(jax.random.PRNGKey(42), M)
-
-    def f(rho, sample):
+    def f(rho, scale_sample):
+        scale, sample = scale_sample
         states = jnp.arange(M)
         discarded = jnp.arange(1<<2)
         last_state = (discarded << (L-2)) | (states[..., jnp.newaxis] >> 2)
         output = alphabet[permutation[states[..., jnp.newaxis]]]
-        loss = rho[last_state] + (sample-output)**2
+        loss = rho[last_state] + scale*(sample-output)**2
 
         next_rho = jnp.min(loss, axis=-1)
         optimal_discarded = jnp.argmin(loss, axis=-1)
@@ -30,23 +29,21 @@ def encode(samples, alphabet):
 
     rho = jnp.full((M,), jnp.inf)
     rho = rho.at[0].set(0)
-    rho, (prev_states, prev_inputs) = jax.lax.scan(f, rho, samples)
+    rho, (prev_states, prev_inputs) = jax.lax.scan(f, rho, (scales, samples))
 
     def g(state, prev):
         prev_state, prev_input = prev
         return prev_state[state], prev_input[state]
 
     last_state = jnp.argmin(rho)
-    _, encoded_rev = jax.lax.scan(g, last_state, (jnp.flipud(prev_states), jnp.flipud(prev_inputs)))
-    encoded = jnp.flipud(encoded_rev)
+    zero, quantized_rev = jax.lax.scan(g, last_state, (jnp.flipud(prev_states), jnp.flipud(prev_inputs)))
+    quantized = jnp.flipud(quantized_rev)
 
-    return encoded
+    return quantized
 
 
-def decode(encoded, alphabet):
+def dequantize(permutation, alphabet, quantized):
     M, = alphabet.shape
-
-    permutation = jax.random.permutation(jax.random.PRNGKey(42), M)
 
     def f(state, input_):
         next_state = (M-1) & ((state<<2) | input_)
@@ -54,32 +51,33 @@ def decode(encoded, alphabet):
         return next_state, output
 
     init_state = 0
-    _, decoded = jax.lax.scan(f, init_state, encoded)
+    _, dequantized = jax.lax.scan(f, init_state, quantized)
 
-    return decoded
+    return dequantized
     
 
-def evaluate(samples, alphabet):
-    encoded = encode(samples, alphabet)
-    decoded = decode(encoded, alphabet)
-    residual = samples - decoded
+def evaluate(permutation, alphabet, scales, samples):
+    quantized = quantize(permutation, alphabet, scales, samples)
+    dequantized = dequantize(permutation, alphabet, quantized)
+    residual = samples - dequantized
     mse = jnp.mean(residual**2)
 
-    bincount = jnp.bincount(encoded, length=4)
+    bincount = jnp.bincount(quantized, length=4)
     dist = bincount / jnp.sum(bincount)
     entropy = -jnp.sum(dist * jnp.log2(dist))
 
     return mse, entropy
 
 
-def train(key, alphabets, block_size, learning_rate, n_steps):
+def train(key, permutation, alphabets, block_size, learning_rate, n_steps):
 
     @jax.jit
     def train_step(key_step, ab, opt_state):
         ab = (ab - jnp.flipud(ab))/2    # enforce alphabet symmetry
         grad_fn = jax.value_and_grad(evaluate, argnums=1, has_aux=True)
         samples = jax.random.normal(key_step, (block_size,))
-        (mse, entropy), grads = grad_fn(samples, ab)
+        scales = jnp.ones_like(samples)
+        (mse, entropy), grads = grad_fn(permutation, ab, scales, samples)
         updates, opt_state = gradient_transform.update(grads, opt_state, ab)
         ab = optax.apply_updates(ab, updates)
 
@@ -106,19 +104,21 @@ def train(key, alphabets, block_size, learning_rate, n_steps):
 
 
 def main(block_size, learning_rate, n_steps):
-    L = 21
+    L = 16
     M = 1<<L
+    permutation = jax.random.permutation(jax.random.PRNGKey(42), M)
     alphabet = jsp.stats.norm.ppf((2*jnp.arange(M)+1)/2/M)
     print("Before", alphabet)
 
     key = jax.random.PRNGKey(42)
     key_train, key_test = jax.random.split(key)
 
-    # alphabet = train(key_train, alphabet, block_size, learning_rate, n_steps)
+    # alphabet = train(key_train, permutation, alphabet, block_size, learning_rate, n_steps)
     # print("After", alphabet)
 
-    samples = jax.random.normal(key_test, (2**18//block_size, block_size))
-    mse_all, entropy_all = jax.lax.map(partial(evaluate, alphabet=alphabet), samples)
+    samples = jax.random.normal(key_test, (2**20//block_size, block_size))
+    scales = jnp.ones(block_size)
+    mse_all, entropy_all = jax.lax.map(partial(evaluate, permutation, alphabet, scales), samples)
     mse_mean = jnp.mean(mse_all).item()
     mse_std = jnp.std(mse_all).item()
     entropy_mean = jnp.mean(entropy_all).item()
